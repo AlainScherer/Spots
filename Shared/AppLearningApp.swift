a7cf205911aaaa62c61332cb71a873f571a6b871
Alain Scherer <a.scherer@47nord.ch>
2020-08-17 20:25:46 +0200

    Initial Commit

---
+++ Shared/AppLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppLearningApp.swift
+//  Shared
+//
+//  Created by Alain Scherer on 17.08.20.
+//
+
+import SwiftUI
+
+@main
+struct AppLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
